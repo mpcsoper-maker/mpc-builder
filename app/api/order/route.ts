6443d6a568b574522a01e8ccb85eb3915973ee6e
mpcsoper-maker <mpcsoper@gmail.com>
2026-01-10 07:17:18 +0100

    Remove @ alias for db import

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { db } from "@/lib/db";
+import { db } from "../../../lib/db";
 import { randomUUID } from "crypto";
 
 export async function POST(req: Request) {
