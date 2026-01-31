2110be79ba1f2ceb417a35ba00e96d91cd9ceabc
mpcsoper-maker <mpcsoper@gmail.com>
2026-01-31 00:00:56 +0000

    Update project files

--- app/lib/prebuilts.ts
+++ app/lib/prebuilts.ts
@@ -16,18 +16,18 @@ export type Prebuilt = {
 export const PREBUILTS: Prebuilt[] = [
   {
     slug: "xxx-1",
-    name: "budget work/gameing pc",
-    price: 523,
-    discount: 40, // ✅ CHANGE THIS (example: 50€ off)
+    name: "Mpcs mid-tier gaming pc",
+    price: 1600,
+    discount: 20,
     image: null,
     parts: [
-      { category: "CPU", name: "E5 2680 v2 xeon", price: 30 },
-      { category: "GPU", name: "rx 580", price: 103 },
-      { category: "RAM", name: "32", price: 100 },
-      { category: "Storage", name: "1 tb ssd", price: 100 },
-      { category: "Motherboard", name: "soyo", price: 40 },
-      { category: "PSU", name: "XXX", price: 80 },
-      { category: "Case", name: "XXX", price: 70 },
+      { category: "CPU", name: "Ryzen 5 7600", price: 175 },
+      { category: "GPU", name: "MSI GeForce RTX 5060 8G", price: 340 },
+      { category: "RAM", name: "Corsair Vengeance Grau 32GB Kit (2x16GB) DDR5-6000 CL36 EXPO", price: 500 },
+      { category: "Storage", name: "1 tb 990 pro", price: 199,},
+      { category: "Motherboard", name: "msi B605 tomahawk", price: 180 },
+      { category: "PSU", name: "be quiet! 750W", price: 85 },
+      { category: "Case", name: "NZXT H5 Flow (2024) Black", price: 100 },
     ],
   },
 ];
