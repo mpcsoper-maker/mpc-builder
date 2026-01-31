1f5b2bfb830bafcc2cce7a9ba60bb7aa22d43c2e
mpcsoper-maker <mpcsoper@gmail.com>
2026-01-31 00:08:38 +0000

    Update project files

--- app/lib/prebuilts.ts
+++ app/lib/prebuilts.ts
@@ -17,12 +17,12 @@ export const PREBUILTS: Prebuilt[] = [
   {
     slug: "xxx-1",
     name: "Mpcs mid-tier gaming pc",
-    price: 1600,
+    price: 1910,
     discount: 20,
     image: null,
     parts: [
       { category: "CPU", name: "Ryzen 5 7600", price: 175 },
-      { category: "GPU", name: "MSI GeForce RTX 5060 8G", price: 340 },
+      { category: "GPU", name: " MSI GeForce RTX 5070 12G", price: 669 },
       { category: "RAM", name: "Corsair Vengeance Grau 32GB Kit (2x16GB) DDR5-6000 CL36 EXPO", price: 500 },
       { category: "Storage", name: "1 tb 990 pro", price: 199,},
       { category: "Motherboard", name: "msi B605 tomahawk", price: 180 },
