6c890a07cf32f5f2749fbf74c04c927831b654f3
mpcsoper-maker <mpcsoper@gmail.com>
2026-02-01 03:54:08 +0000

    Update project files

--- app/lib/prebuilts.ts
+++ app/lib/prebuilts.ts
@@ -26,8 +26,8 @@ export const PREBUILTS: Prebuilt[] = [
       { category: "RAM", name: "Crucial Pro OC Schwarz 32GB Kit (2x16GB) DDR5-6000 CL36", price: 399 },
       { category: "Storage", name: "1 tb 990 pro", price: 199,},
       { category: "Motherboard", name: "msi B605 tomahawk", price: 180 },
-      { category: "PSU", name: "mpcs 7502", price: - },
-      { category: "Case", name: "m-pcs case", price: - },
+      { category: "PSU", name: "mpcs 7502", price: 0 },
+      { category: "Case", name: "m-pcs case", price: 0 },
     ],
   },
   {
@@ -42,8 +42,8 @@ export const PREBUILTS: Prebuilt[] = [
       { category: "RAM", name: "Crucial Pro OC Schwarz 32GB Kit (2x16GB) DDR5-6000 CL36", price: 399 },
       { category: "Storage", name: "1 tb 990 pro", price: 199,},
       { category: "Motherboard", name: "msi B605 tomahawk", price: 180 },
-      { category: "PSU", name: "m-pcs 750w", price: - },
-      { category: "Case", name: "m-pcs case", price: - },
+      { category: "PSU", name: "m-pcs 750w", price: 0 },
+      { category: "Case", name: "m-pcs case", price: 0 },
     ],
   },
 ];
