6179559407312f7c85dc9aa8374627a4faa0be05
mpcsoper-maker <mpcsoper@gmail.com>
2026-02-01 04:35:09 +0000

    Update project files

--- app/lib/prebuilts.ts
+++ app/lib/prebuilts.ts
@@ -40,7 +40,7 @@ export const PREBUILTS: Prebuilt[] = [
       { category: "CPU", name: "Ryzen 5 7600", price: 175 },
       { category: "GPU", name: " MSI GeForce RTX 5060 8G", price: 340 },
       { category: "RAM", name: "Crucial Pro OC Schwarz 32GB Kit (2x16GB) DDR5-6000 CL36", price: 399 },
-      { category: "Storage", name: "1 tb 990 pro", price: 199,},
+      { category: "Storage", name: "1 tb 990 pro", price: 299,},
       { category: "Motherboard", name: "msi B605 tomahawk", price: 180 },
       { category: "PSU", name: "m-pcs 750w", price: 0 },
       { category: "Case", name: "m-pcs case", price: 0 },
