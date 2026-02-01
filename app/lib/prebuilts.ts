23008447ab6f7f32fd2c3fe572b7676f991d556e
mpcsoper-maker <mpcsoper@gmail.com>
2026-02-01 03:50:03 +0000

    Update project files

--- app/lib/prebuilts.ts
+++ app/lib/prebuilts.ts
@@ -42,8 +42,8 @@ export const PREBUILTS: Prebuilt[] = [
       { category: "RAM", name: "Crucial Pro OC Schwarz 32GB Kit (2x16GB) DDR5-6000 CL36", price: 399 },
       { category: "Storage", name: "1 tb 990 pro", price: 199,},
       { category: "Motherboard", name: "msi B605 tomahawk", price: 180 },
-      { category: "PSU", name: "be quiet! 750W", price: 85 },
-      { category: "Case", name: "NZXT H5 Flow (2024) Black", price: 100 },
+      { category: "PSU", name: "m-pcs 750w", price: 85 },
+      { category: "Case", name: "m-pcs case", price: 100 },
     ],
   },
 ];
